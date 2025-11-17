92264d2c5dd447df9f1a7dc11bba5d316327f7a3
Nicolas Belaiche <nicolas.bellaiche@ign.fr>
2025-11-17 11:58:00 +0100

    WIP

--- deploy/render/env.sh
+++ deploy/render/env.sh
@@ -8,6 +8,6 @@ export NO_AUTH=0
 # URL publique des services REST KinoCaen
 RENDER_FLASK_ID="gunicorn-hh3p"
 
-export API_REST_URL=https://kinocaen.alwaysdata.net
+export API_REST_URL=https://inventory-service.alwaysdata.net/
 export API_SECURITY_REST_URL=https://${RENDER_FLASK_ID}.onrender.com
 
