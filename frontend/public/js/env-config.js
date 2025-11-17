df45a43b5e517546d8379aa44b4efe3f1045570b
Nicolas Bellaiche <nbellaiche@mac2401w005.ign.fr>
2025-11-17 19:38:39 +0100

    WIP

--- frontend/public/js/env-config.js
+++ frontend/public/js/env-config.js
@@ -3,7 +3,8 @@ window.ENV = {
   BASE_PATH: "",
   API_REST_URLS: [
     "http://127.0.0.1:5000",
-    "https://inventory-service.alwaysdata.net"
+    "https://inventory-service.alwaysdata.net",
+    ""
   ],
   SERVICE_WAKEUP_INTERVAL: 720
 };
