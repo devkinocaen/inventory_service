dab49eb8b47981ec374d2730021915ac7f1f0654
Nicolas Belaiche <nicolas.bellaiche@ign.fr>
2025-11-17 11:53:12 +0100

    WIP

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,12 +1,19 @@
-# passenger_wsgi.py
 import sys
 import os
 
-# Ajouter le dossier courant au path Python
-sys.path.insert(0, os.path.dirname(__file__))
+# Chemin vers le projet
+project_home = os.path.dirname(__file__)
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
-# Import de l'app Flask
+# --- Forcer le venv Python ---
+venv_path = os.path.join(project_home, "venv")
+sys.path.insert(0, os.path.join(venv_path, "lib/python3.12/site-packages"))  # adapte la version
+sys.prefix = venv_path
+sys.exec_prefix = venv_path
+
+# Import de l'application Flask
 from app_flask import app
 
-# Alwaysdata et Gunicorn attendent un objet nommé "application"
+# Passenger attend un objet nommé "application"
 application = app
