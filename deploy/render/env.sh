c1b00828f9150eac618abf380f41fa2031f062f6
Nicolas Bellaiche <nbellaiche@mac2401w005.ign.fr>
2025-11-17 18:30:46 +0100

    WIP

--- deploy/render/env.sh
+++ deploy/render/env.sh
@@ -9,5 +9,6 @@ export NO_AUTH=0
 RENDER_FLASK_ID="gunicorn-hh3p"
 
 export API_REST_URL=https://inventory-service.alwaysdata.net/
-export API_SECURITY_REST_URL=https://${RENDER_FLASK_ID}.onrender.com
+#export API_SECURITY_REST_URL=https://${RENDER_FLASK_ID}.onrender.com
+export API_SECURITY_REST_URL=
 
