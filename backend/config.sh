fbd87392228e550909352fe0ad0d60853847af0f
Nicolas Bellaiche <nbellaiche@mac2401w005.ign.fr>
2025-11-18 15:46:21 +0100

    WIP

--- backend/config.sh
+++ backend/config.sh
@@ -21,5 +21,5 @@ fi
 
 
 export ALWAYS_DATA_API_REST_URL=https://inventory-service.alwaysdata.net
-#export RENDER_API_REST_URL=https://gunicorn-hh3p.onrender.com
-export RENDER_API_REST_URL=
+#export RENDER_API_REST_URL=
+export RENDER_API_REST_URL=https://inventory-service-tz0g.onrender.com
