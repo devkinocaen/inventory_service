e1c626ffeb62b2ca2f66c0f078d75b5f2cd68390
Nicolas Belaiche <nicolas.bellaiche@ign.fr>
2025-11-26 01:40:33 +0100

    WIP

---
+++ frontend/public/js/env-config.js
@@ -0,0 +1,19 @@
+window.ENV = {
+  APP_NAME: "Costumerie",
+  APP_VERSION: "dev",
+  DB_CLIENT: "",
+  BASE_PATH: "",
+
+  // Variables spécifiques à chaque site Vercel
+  DB_NAME: "",
+  HEADER_IMAGE_URL: "./images/bandeau_costumerie_julie.png",
+
+  // URLs API déjà présentes dans ta version
+  API_REST_URLS: [
+    "http://127.0.0.1:5000",
+    "https://inventory-service.alwaysdata.net",
+    "https://inventory-service-tz0g.onrender.com"
+  ],
+
+  SERVICE_WAKEUP_INTERVAL: 720
+};
