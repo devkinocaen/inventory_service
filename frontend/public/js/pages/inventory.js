0a66a9a471fdaaa0eddf4f59ce2fe862541ed18c
Nicolas Bellaiche <nbellaiche@mac2401w005.ign.fr>
2025-12-08 11:20:33 +0100

    WIP

--- frontend/public/js/pages/inventory.js
+++ frontend/public/js/pages/inventory.js
@@ -214,7 +214,13 @@ function initEditableCells() {
     if (['gender','status','quality','category','subcategory'].includes(field)) return;
     td.addEventListener('dblclick', async () => {
       const oldValue = td.textContent;
-      const input = document.createElement('input');
+      
+      oldValue = oldValue.replace(
+       /([\u2700-\u27BF]|[\uE000-\uF8FF]|[\uD83C-\uDBFF\uDC00-\uDFFF]|\u24C2|\uD83D[\uDC00-\uDE4F]|\uD83D[\uDE80-\uDEFF])/g,
+        ''
+      ).trim();
+    
+        const input = document.createElement('input');
       input.type = field === 'price_per_day' ? 'number' : 'text';
       input.value = oldValue;
       td.textContent = '';
